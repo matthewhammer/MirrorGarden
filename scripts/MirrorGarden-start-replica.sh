2d3a8239ca4aa818c15fef45acc66ef588d2b041
Matthew Hammer <matthew@dfinity.org>
2020-11-24 13:22:16 -0700

    default demo setup benefits from two users

--- scripts/MirrorGarden-start-replica.sh
+++ scripts/MirrorGarden-start-replica.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 VERSION=`cat .DFX_VERSION`
 export PATH=~/.cache/dfinity/versions/$VERSION:`pwd`:$PATH
+echo
+echo Starting: Mirror Garden 2-user demo...using DFINITY SDK replica
+echo
 dfx stop &&\
 dfx start --background --clean &&\
 dfx canister create MirrorGarden &&\
 dfx build MirrorGarden &&\
-dfx canister install MirrorGarden ||\
-dfx canister install MirrorGarden --mode=reinstall &&\
-ic-gt -h &&\
-ic-gt connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Alice", (100, 255, 100))'
-
+dfx canister install MirrorGarden
+ic-gt connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Alice", (100, 200, 200))' &
+ic-gt connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Bob", (200, 100, 200))' &
+echo
+echo Mirror Garden 2-user demo, started.
+echo
