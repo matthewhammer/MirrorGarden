7a3e9c47be824b8af160359a719ecf6e78551d43
Matthew Hammer <matthew@dfinity.org>
2021-01-06 12:30:43 -0700

    try mo-doc in CI

--- scripts/MirrorGarden-test-headless.sh
+++ scripts/MirrorGarden-test-headless.sh
@@ -5,6 +5,5 @@ dfx stop &&\
 dfx start --background --clean &&\
 dfx canister create MirrorGarden &&\
 dfx build MirrorGarden &&\
-dfx canister install MirrorGarden ||\
-dfx canister install MirrorGarden --mode=reinstall &&\
-echo "to do -- download and run ic-mt using some captured user input/output"
+mo-doc &&\
+echo "Success!"
