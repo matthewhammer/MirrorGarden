2b14c505b2bbcad5edfeb565285ecdabc8c41ced
Matthew Hammer <matthew@dfinity.org>
2021-01-12 14:00:26 -0700

    invoke mo-doc

--- scripts/MirrorGarden-test-headless.sh
+++ scripts/MirrorGarden-test-headless.sh
@@ -7,4 +7,6 @@ dfx canister create MirrorGarden &&\
 dfx build MirrorGarden &&\
 dfx canister install MirrorGarden ||\
 dfx canister install MirrorGarden --mode=reinstall &&\
-echo "to do -- download and run ic-mt using some captured user input/output"
+echo "to do -- download and run ic-mt using some captured user input/output" &&\
+mo-doc &&\
+echo "Success!"
