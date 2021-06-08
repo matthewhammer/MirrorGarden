a241df56388b285aa0ddf6dca6207be3e387a83e
Matthew Hammer <matthew.hammer@gmail.com>
2021-06-08 10:55:54 -0600

    elim double dfx start; double deploy

--- scripts/MirrorGarden-test-headless.sh
+++ scripts/MirrorGarden-test-headless.sh
@@ -3,8 +3,7 @@ VERSION=`cat .DFX_VERSION`
 export PATH=~/.cache/dfinity/versions/$VERSION:`pwd`:$PATH
 set -e
 set -x
-dfx start --background
-dfx deploy --no-wallet MirrorGarden
+
 ic-mt --help
 echo "to do -- run ic-mt on test vectors"
 echo "Success!"
