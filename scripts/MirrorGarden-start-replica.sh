e103422c40b1cf426e0af35a2020956230dd7c05
Matthew Hammer <matthew.hammer@gmail.com>
2021-07-12 15:29:54 -0600

    Chore: Fix scripts and upates for dfx

--- scripts/MirrorGarden-start-replica.sh
+++ scripts/MirrorGarden-start-replica.sh
@@ -7,11 +7,11 @@ echo
 set -x
 set -e
 dfx stop
-dfx start --background --clean
+dfx start --no-artificial-delay --background --clean
 dfx deploy --no-wallet MirrorGarden
 
-ic-mt connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Alice", (100, 200, 200))' &
-ic-mt connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Bob", (200, 100, 200))' &
+ic-mt connect http://127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Alice", (100, 200, 200))' &
+ic-mt connect http://127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Bob", (200, 100, 200))' &
 echo
 echo Mirror Garden 2-user demo, started.
 echo
--- scripts/MirrorGarden-test-headless.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VERSION=`cat .DFX_VERSION`
-export PATH=~/.cache/dfinity/versions/$VERSION:`pwd`:$PATH
-set -e
-set -x
-
-ic-mt --help
-echo "to do -- run ic-mt on test vectors"
-echo "Success!"
